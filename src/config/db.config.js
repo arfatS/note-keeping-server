025f6eab863c233ce169671d82505a2aed4c5dad
arfatS <arfatsarole@gmail.com>
2021-05-07 21:23:34 +0530

    changes

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: "https://as-note-keeping-server.herokuapp.com/",
+    HOST: process.env.HOST || "localhost",
     USER: process.env.USER || "root",
     PASSWORD: process.env.PASSWORD ||  "",
     DB: process.env.DB || "note-keeping",
