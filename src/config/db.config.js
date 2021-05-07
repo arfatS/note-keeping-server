191491e2f6344bdfe6374f132775edf176195527
arfatS <arfatsarole@gmail.com>
2021-05-07 21:37:26 +0530

    changes

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,7 +3,7 @@ module.exports = {
     USER: process.env.USER || "root",
     PASSWORD: process.env.PASSWORD ||  "",
     DB: process.env.DB || "note-keeping",
-    dialect: "mysql",
+    dialect: "postgres",
     pool: {
         max: 5,
         min: 0,
