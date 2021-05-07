48decbb8b15a5687781963544fd0420f582175b5
arfatS <arfatsarole@gmail.com>
2021-05-07 20:45:29 +0530

    changes

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "note-keeping",
+    HOST: process.env.HOST || "localhost",
+    USER: process.env.USER || "root",
+    PASSWORD: process.env.PASSWORD ||  "",
+    DB: process.env.DB || "note-keeping",
     dialect: "mysql",
     pool: {
         max: 5,
