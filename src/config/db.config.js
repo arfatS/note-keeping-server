70d4a10633e47695d7817fa7a8bd175fcadb94dd
arfatS <arfatsarole@gmail.com>
2021-05-07 20:56:35 +0530

    ..

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: process.env.HOST || "localhost",
+    HOST: "https://as-note-keeping-server.herokuapp.com/",
     USER: process.env.USER || "root",
     PASSWORD: process.env.PASSWORD ||  "",
     DB: process.env.DB || "note-keeping",
